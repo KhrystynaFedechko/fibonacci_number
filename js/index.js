09398b7b78051acc4974bfeaed28bf5ba21a32c5
Khrystyna <khrystynadyka@gmail.com>
2016-11-09 20:00:15 +0000

    add m2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
+// function methodR(n) {
+// if ( n <= 2 ) {
+//     return 1;
+// } 
+// return methodR ( n - 1 ) + methodR ( n - 2 );
+// } 
+// var x = Number(prompt('Imput number'));
+// var result = methodR(x);
+// document.write("The Fibonacci number is " + result);
 
-    function lopping(n) {
-    var f;
+    function methodLoop(n) {
+    var a = 0, b = 1, f = 1;
     for(var i = 2; i <= n; i++) {
-    f = (n-2) + (n-1);
+        f = a + b;
+        a = b;
+        b = f;
     }
     return f;
     }
-    var f = Number(prompt('Imput n'));
-    var result = lopping(f);
-    document.write(result);
+    var x = Number(prompt('Imput number'));
+    var result = methodLoop(x);
+    document.write("The Fibonacci number is " + result);
+
+
+   
+    
+    
+  
